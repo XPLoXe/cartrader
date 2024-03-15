3173bb6ad5e419a15ab8a7cd3069259cb03b1878
Alejandro Bocchicchio <aleb2800@gmail.com>
2024-03-15 09:36:17 +0100

    Adding Cards and SideBar components to Dynamic make.vue route

--- pages/city/[city]/car/[[make]].vue
+++ pages/city/[city]/car/[[make]].vue
@@ -1,3 +1,17 @@
+import { CarCards } from '#build/components'; import { CarCard } from
+'#build/components'; import { CarCard } from '#build/components'; import {
+CarSideBar } from '#build/components';
 <template>
-  <div>City and Make</div>
+  <div>
+    <NavBar />
+    <!-- CARS PAGE -->
+    <div
+      class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5"
+    >
+      <div class="mt-32 flex">
+        <CarSideBar />
+        <CarCards />
+      </div>
+    </div>
+  </div>
 </template>
