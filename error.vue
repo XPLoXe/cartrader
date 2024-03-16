512b891e999ef44fbb3dc4173a61eaa71161689f
Alejandro Bocchicchio <aleb2800@gmail.com>
2024-03-16 10:44:41 +0100

    Applying button custom class

--- error.vue
+++ error.vue
@@ -2,12 +2,7 @@
   <div class="flex h-screen justify-center items-center flex-col">
     <h1 class="text-9xl">{{ error?.statusCode }}</h1>
     <p class="mt-7 text-2xl">{{ error?.statusMessage }}</p>
-    <button
-      @click="goBack"
-      class="mt-7 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-    >
-      Go back
-    </button>
+    <button @click="goBack" class="btn">Go back</button>
   </div>
 </template>
 
