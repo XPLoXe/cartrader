5c5914a99aad8071bd105b6cb7b472cb0d685846
Alejandro Bocchicchio <aleb2800@gmail.com>
2024-03-16 09:49:47 +0100

    Dynamically render error

--- error.vue
+++ error.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex h-screen justify-center items-center flex-col">
-    <h1 class="text-9xl">404</h1>
-    <p class="mt-7 text-2xl">Page not found</p>
+    <h1 class="text-9xl">{{ error?.statusCode }}</h1>
+    <p class="mt-7 text-2xl">{{ error?.statusMessage }}</p>
     <button
       @click="goBack"
       class="mt-7 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
@@ -12,6 +12,8 @@
 </template>
 
 <script setup>
+const error = useError();
+
 const goBack = () => {
   navigateTo("/");
 };
