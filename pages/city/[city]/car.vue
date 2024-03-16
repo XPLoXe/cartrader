07f227b046ab6edc01fc5a2569c73fb50f062faf
Alejandro Bocchicchio <aleb2800@gmail.com>
2024-03-16 12:02:00 +0100

    Handling Client Side Error

--- pages/city/[city]/car.vue
+++ pages/city/[city]/car.vue
@@ -3,8 +3,21 @@
     <!-- CARS PAGE -->
 
     <div class="mt-32 flex">
-      <CarSideBar />
-      <NuxtPage />
+      <NuxtErrorBoundary>
+        <CarSideBar />
+        <NuxtPage />
+        <template #error="{ error }">
+          <div class="text-center mx-auto flex flex-col">
+            <h1 class="text-3xl text-red-600 my-4">
+              Sorry, something went wrong :(
+            </h1>
+            <code>{{ error }}</code>
+            <button class="btn max-w-48 mx-auto" @click="error.value = null">
+              Go Back
+            </button>
+          </div></template
+        >
+      </NuxtErrorBoundary>
     </div>
   </div>
 </template>
