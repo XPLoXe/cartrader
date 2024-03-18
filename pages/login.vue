2660e6cd7920943fb3c845ec0357b6d6ccdbb582
Alejandro Bocchicchio <aleb2800@gmail.com>
2024-03-18 10:16:43 +0100

    Login page added with GitHub Login

---
+++ pages/login.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="mt-10">
+    <h1 class="text-5xl font-bold mb-7">Log In</h1>
+    <button @click="login" class="btn">Login</button>
+  </div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: "custom",
+});
+
+const supabase = useSupabaseClient();
+
+const user = useSupabaseUser();
+
+const login = async () => {
+  const { error } = await supabase.auth.signInWithOAuth({
+    provider: "github",
+  });
+  if (error) {
+    console.log(error, "error with login function");
+  }
+};
+</script>
