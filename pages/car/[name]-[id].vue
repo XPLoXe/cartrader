1c4f285c0760793b490f429c8bdcdbf6902f211b
Alejandro Bocchicchio <aleb2800@gmail.com>
2024-03-21 15:55:57 +0100

    Fetch car by ID

--- pages/car/[name]-[id].vue
+++ pages/car/[name]-[id].vue
@@ -5,27 +5,12 @@
 </template>
 
 <script setup>
-const { cars } = useCars();
-
 const route = useRoute();
+const { data: car } = await useFetchCar(route.params.id);
 const { toTitleCase } = useUtilities();
 useHead({
   title: `${toTitleCase(route.params.name)} in ${route.params.id}`,
 });
-
-//computed property that will take the id from the route and find the car with that id
-const car = computed(() => {
-  return cars.find((c) => {
-    return c.id === parseInt(route.params.id);
-  });
-});
-
-if (!car.value) {
-  throw createError({
-    statusCode: 404,
-    message: `Car with id of ${route.params.id} not found.`,
-  });
-}
 
 definePageMeta({
   layout: "custom",
