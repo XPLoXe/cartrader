888337d6e0d0dc3cd4f4d5f9a14a382dae02f506
Alejandro Bocchicchio <aleb2800@gmail.com>
2024-03-23 09:41:10 +0100

    Fix: price input was not added

--- pages/profile/listings/create.vue
+++ pages/profile/listings/create.vue
@@ -71,24 +71,30 @@ const inputs = [
   },
   {
     id: 3,
+    title: "Price *",
+    name: "price",
+    placeholder: "10000",
+  },
+  {
+    id: 4,
     title: "Miles *",
     name: "miles",
     placeholder: "10000",
   },
   {
-    id: 4,
+    id: 5,
     title: "City *",
     name: "city",
     placeholder: "Austin",
   },
   {
-    id: 5,
+    id: 6,
     title: "Number of Seats *",
     name: "seats",
     placeholder: "5",
   },
   {
-    id: 6,
+    id: 7,
     title: "Features *",
     name: "features",
     placeholder: "Leather Interior, No Accidents",
